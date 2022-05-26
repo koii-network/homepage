abf6efdfce0ad080d2c4c57d466ae40e7362e8a5
Wojciech Jelen <w.jelen20@gmail.com>
2022-05-26 23:51:24 +0200

    example button

--- components/ui/Button.tsx
+++
@@ -1,9 +0,0 @@
-export type ButtonPropsType = Readonly<
-  {
-    label: string;
-  } & HTMLButtonElement
->;
-
-export const Button: React.FC<ButtonPropsType> = ({ label }) => (
-  <button>{label}</button>
-);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from "next";
+import { Button } from "@/components/ui/Button";
 import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button label="Prove it." />
+    </div>
+  );
 };
 
 export default Home;
