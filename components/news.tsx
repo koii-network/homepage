a30fc335f83cb76151c672f782fd5575dc98a97f
somali0128 <ldydq970128@gmail.com>
2022-05-28 01:09:24 -0300

    update news section

--- components/news.tsx
+++ components/news.tsx
@@ -3,7 +3,7 @@ import { Newsblock } from "./ui/News"
 
 export const News = () => {
     return (
-       
+       <div>
       <Newsblock
       cover= "/images/news1.png"
       title="Growing Attention Economy"
@@ -12,5 +12,22 @@ export const News = () => {
       url="https://cointelegraph.com/news/nft-potential-takes-quantum-leap-with-koii-network-s-new-dynamic-nft-standard"
       urltext="Permaweb Pioneers podcast"
       />
+      <Newsblock
+      cover= "/images/news1.png"
+      title="Growing Attention Economy"
+      content='"What were trying to do is complement that [proof of humanity] with proof of contribution. The way you prove your contribution to the collective knowledge commons is by getting attention from other people who have a higher reputation."'
+      logo=""
+      url="https://cointelegraph.com/news/nft-potential-takes-quantum-leap-with-koii-network-s-new-dynamic-nft-standard"
+      urltext="Permaweb Pioneers podcast"
+      />
+      <Newsblock
+      cover= "/images/news1.png"
+      title="Growing Attention Economy"
+      content='"What were trying to do is complement that [proof of humanity] with proof of contribution. The way you prove your contribution to the collective knowledge commons is by getting attention from other people who have a higher reputation."'
+      logo=""
+      url="https://cointelegraph.com/news/nft-potential-takes-quantum-leap-with-koii-network-s-new-dynamic-nft-standard"
+      urltext="Permaweb Pioneers podcast"
+      />
+      </div>
     );
 };
