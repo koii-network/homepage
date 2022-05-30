dad7c40998fd2a26334a3097f9c809e5f1786bd8
kjesien <krzysztofjesien@gmail.com>
2022-05-30 17:18:54 +0200

    Eslint fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import type { NextPage } from "next";
-import { Button } from "@/components/ui/Button";
-import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return <div>Page content</div>;
