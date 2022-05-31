7f808d38d6f85707e3113c366038300fc4023cfe
somali0128 <ldydq970128@gmail.com>
2022-05-31 17:53:34 -0300

    fix lint error

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -3,25 +3,29 @@ import Hero2 from "./hero2";
 import Hero3 from "./hero3";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from "../../styles/carousel.module.scss";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
 const Hero = () => {
   return (
-    <div className={`
+    <div
+      className={`
       ${styles.dot}
       ${styles.selectdot}
       ${styles.controldot}
       ${styles.slide}
-      `}>
-    <Carousel
-    showThumbs={false}
-    showStatus={false}
-    autoPlay={true}
->
-      <div><Hero1 /></div>
-      <div><Hero2 /></div>
-      <div><Hero3 /></div>
-    </Carousel>
+      `}
+    >
+      <Carousel showThumbs={false} showStatus={false} autoPlay={true}>
+        <div>
+          <Hero1 />
+        </div>
+        <div>
+          <Hero2 />
+        </div>
+        <div>
+          <Hero3 />
+        </div>
+      </Carousel>
     </div>
   );
 };
