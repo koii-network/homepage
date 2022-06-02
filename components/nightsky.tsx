1679dcf52f20efc027bbc9af861269f1abda9e6a
somali0128 <ldydq970128@gmail.com>
2022-06-02 13:31:08 -0300

    fix lint

--- components/nightsky.tsx
+++ components/nightsky.tsx
@@ -1,6 +1,5 @@
 import styles from "/styles/home.module.css";
 import Image from "next/image";
-import icebergtop from "/public/images/icebergtop.svg";
 import iceberg from "/public/images/iceberg.svg";
 import moon from "/public/images/moon.svg";
 import birds from "/public/images/birds.svg";
@@ -35,16 +34,30 @@ export const NightSky = () => {
         </div>
       </div>
       <div className="star-text md:text-2xl flex md:pl-[35rem] text-white w-auto">
-          <span className="relative bottom-[16rem] md:bottom-[3rem] left-[12rem] md:left-[5rem]">Identity</span>
-          <span className="relative bottom-[10rem] md:left-[5rem] left-[13rem] md:top-[4rem] opacity-40">Creativity</span>
-          <span className="relative right-[5rem] md:right-[30rem] bottom-[1rem] md:top-4[rem] opacity-20">Content</span>
-          <span className="relative top-[5rem] md:top-[11rem] md:right-[31rem] opacity-80">Rewards</span>
-          <span className="relative top-[8rem] md:top-[15rem] right-[15rem] md:right-[54rem] opacity-60">Attention</span>
+        <span className="relative bottom-[16rem] md:bottom-[3rem] left-[12rem] md:left-[5rem]">
+          Identity
+        </span>
+        <span className="relative bottom-[10rem] md:left-[5rem] left-[13rem] md:top-[4rem] opacity-40">
+          Creativity
+        </span>
+        <span className="relative right-[5rem] md:right-[30rem] bottom-[1rem] md:top-4[rem] opacity-20">
+          Content
+        </span>
+        <span className="relative top-[5rem] md:top-[11rem] md:right-[31rem] opacity-80">
+          Rewards
+        </span>
+        <span className="relative top-[8rem] md:top-[15rem] right-[15rem] md:right-[54rem] opacity-60">
+          Attention
+        </span>
       </div>
-    <div className="icebergtext md:text-2xl flex flex-row-reverse relative text-[#171753]">
-        <span className="relative right-[5rem] md:right-[18rem] top-[13rem] md:top-[13rem]">Free Crypto</span>
-        <span className="relative right-[4rem] md:right-[19rem] top-[19rem] md:top-[24rem]">Amazing Developer Tools</span>
-    </div>
+      <div className="icebergtext md:text-2xl flex flex-row-reverse relative text-[#171753]">
+        <span className="relative right-[5rem] md:right-[18rem] top-[13rem] md:top-[13rem]">
+          Free Crypto
+        </span>
+        <span className="relative right-[4rem] md:right-[19rem] top-[19rem] md:top-[24rem]">
+          Amazing Developer Tools
+        </span>
+      </div>
       <div className="Icebergtop absolute top-[674px] md:top-[622px] right-[-5rem] md:right-0 w-[347px] md:w-[562px] h-[630px] md:h-[1035px]">
         <Image
           className=" "
