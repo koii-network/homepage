0c15b42170984a556fd749b188425c805e2affb6
somali0128 <ldydq970128@gmail.com>
2022-06-02 11:05:57 -0300

    update text position

--- components/nightsky.tsx
+++ components/nightsky.tsx
@@ -1,6 +1,7 @@
 import styles from "/styles/home.module.css";
 import Image from "next/image";
 import icebergtop from "/public/images/icebergtop.svg";
+import iceberg from "/public/images/iceberg.svg";
 import moon from "/public/images/moon.svg";
 import birds from "/public/images/birds.svg";
 
@@ -16,7 +17,7 @@ export const NightSky = () => {
           objectFit="cover"
         />
       </div>
-      <div className="birds absolute top-[115px] md:bottom-0 md:top-[unset] left-[3rem] md:left-[16rem] w-[300px] h-[550px]">
+      <div className="birds absolute top-[115px] md:bottom-0 md:top-[unset] left-[3rem] md:left-[16rem] w-[300px] h-[750px]">
         <Image
           className=""
           src={birds}
@@ -33,15 +34,24 @@ export const NightSky = () => {
           Koii is web3, for everyone.
         </div>
       </div>
-
-      <div className="Icebergtop flex flex-row-reverse">
+      <div className="star-text text-2xl flex flex-col pl-[35rem] text-white ">
+          <span className="relative bottom-[3rem]">Identity</span>
+          <span className="relative left-[11rem] top-[2rem] opacity-40">Creativity</span>
+          <span className="relative right-[19rem] opacity-20">Content</span>
+          <span className="relative top-[5rem] opacity-80">Rewards</span>
+          <span className="relative top-[8rem] right-[26rem] opacity-60">Attention</span>
+      </div>
+    <div className="icebergtext text-2xl flex flex-col relative text-[#171753]">
+        <span className="relative left-[63rem] top-[5rem]">Free Crypto</span>
+        <span className="relative left-[42rem] top-[14rem]">Amazing Developer Tools</span>
+    </div>
+      <div className="Icebergtop absolute top-[622px] right-0 w-[562px] h-[1035px]">
         <Image
-          className="!mr-[-2rem] "
-          src={icebergtop}
+          className=" "
+          src={iceberg}
           alt="icebergtop"
-          width="535"
-          height="365"
-          layout="fixed"
+          layout="fill"
+          objectFit="cover"
         />
       </div>
     </div>
