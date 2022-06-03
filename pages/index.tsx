af09aaab68be11206bff7295e2814af1701d6539
somali0128 <ldydq970128@gmail.com>
2022-06-03 01:12:54 -0300

    update css

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       <Hero />
       <NightSky />
       <Iceberg />
-      <Timeline />
+      {/* <Timeline /> */}
       <News />
       Page content
     </div>
