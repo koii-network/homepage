c1748b256cac4906d38bb249d121eb80e5b1808b
somali0128 <ldydq970128@gmail.com>
2022-06-03 01:13:55 -0300

    fix lint

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -1,12 +1,5 @@
 import styles from "/styles/home.module.css";
-import fishstream from "/public/images/fish-stream_desktop.svg";
-import anchor from "/public/images/anchor.svg";
-import Image from "next/image";
 
 export const Timeline = () => {
-    return (
-        <div className={`${styles.Timeline} h-[900px] md:h-[1485px]`}>
-             
-        </div>
-    )
-}
+  return <div className={`${styles.Timeline} h-[900px] md:h-[1485px]`}></div>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 
 import { NightSky } from "@/components/nightsky";
 import { Iceberg } from "@/components/iceberg";
-import { Timeline } from "@/components/timeline";
 import { News } from "@/components/news";
 import Hero from "@/components/hero";
 
@@ -12,7 +11,6 @@ const Home: NextPage = () => {
       <Hero />
       <NightSky />
       <Iceberg />
-      {/* <Timeline /> */}
       <News />
       Page content
     </div>
