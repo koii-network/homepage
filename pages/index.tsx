a83e073f041b6b5aca167895cf93786daa91c303
Wojciech Jelen <w.jelen20@gmail.com>
2022-06-03 11:32:00 +0200

    pond

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 import { News } from "@/components/news";
 import Hero from "@/components/hero";
+import { Pond } from "@/components/pond/pond";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Hero />
       <News />
+      <Pond />
     </div>
   );
 };
