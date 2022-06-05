89584b00232820cb301a0c94ce3a4e0f2c16ace9
somali0128 <66934242+somali0128@users.noreply.github.com>
2022-06-05 00:37:34 -0300

    Responsive/grant (#17)
    * text responsive
    
    * bubble responsive
    
    * fix lint

--- components/grant-program/GrantProgram.tsx
+++ components/grant-program/GrantProgram.tsx
@@ -1,24 +1,35 @@
 import Image from "next/image";
-
+import { useMediaQuery } from "@/components/hooks";
 import { Button } from "@/components/ui/Button";
 import { Bubble } from "./Bubble";
 import Link from "next/link";
 
 export const GrantProgram = () => {
+  const isBreakpoint = useMediaQuery(768);
   return (
-    <section className="grant-program h-[696px]">
+    <section className="grant-program h-[920px] md:h-[696px]">
       <div className="flex flex-row max-w-[1140px] md:mx-auto justify-between">
         <div className="flex flex-col">
           <div className="mb-20">
-            <h2 className="text-4xl leading-[48px] pt-28 text-white pb-3">
-              How will you
-              <br />
-              change the world?
-            </h2>
-            <p className="text-sm text-white">Get in on Koii’s</p>
-            <p className="text-2xl text-gold">$1M Grant Program</p>
+            {isBreakpoint ? (
+              <h2 className="text-2xl leading-[48px] pt-28 px-12 text-white text-center pb-3">
+                How will you change the world?
+              </h2>
+            ) : (
+              <h2 className="text-4xl leading-[48px] pt-28 text-white pb-3">
+                How will you
+                <br />
+                change the world?
+              </h2>
+            )}
+            <p className="text-sm text-white text-center md:text-left">
+              Get in on Koii’s
+            </p>
+            <p className="text-2xl text-gold text-center md:text-left">
+              $1M Grant Program
+            </p>
           </div>
-          <div>
+          <div className="mt-[22rem] text-center">
             <Link href="https://koii.network/form/grant/">
               <a>
                 <Button
@@ -71,24 +82,66 @@ export const GrantProgram = () => {
               height={19}
             />
           </div>
-          <div className="absolute top-[60px] right-[240px]">
-            <Bubble
-              width={280}
-              height={280}
-              text={"Community Governed Social Media"}
-            />
-          </div>
-          <div className="absolute top-[140px] right-[0px]">
-            <Bubble width={280} height={280} text={"Transparent Ad Networks"} />
-          </div>
 
-          <div className="absolute top-[340px] right-[240px]">
-            <Bubble width={223} height={223} text={"Search Without Serfdom"} />
-          </div>
+          {isBreakpoint ? (
+            <div>
+              <div className="absolute top-[260px] right-[160px]">
+                <Bubble
+                  width={200}
+                  height={200}
+                  text={"Community Governed Social Media"}
+                />
+              </div>
+              <div className="absolute top-[360px] right-[0px]">
+                <Bubble
+                  width={200}
+                  height={200}
+                  text={"Transparent Ad Networks"}
+                />
+              </div>
 
-          <div className="absolute top-[450px] right-[0px]">
-            <Bubble width={175} height={175} text={"And more"} />
-          </div>
+              <div className="absolute top-[460px] right-[150px]">
+                <Bubble
+                  width={180}
+                  height={180}
+                  text={"Search Without Serfdom"}
+                />
+              </div>
+
+              <div className="absolute top-[560px] right-[40px]">
+                <Bubble width={135} height={135} text={"And more"} />
+              </div>
+            </div>
+          ) : (
+            <div>
+              <div className="absolute top-[60px] right-[240px]">
+                <Bubble
+                  width={280}
+                  height={280}
+                  text={"Community Governed Social Media"}
+                />
+              </div>
+              <div className="absolute top-[140px] right-[0px]">
+                <Bubble
+                  width={280}
+                  height={280}
+                  text={"Transparent Ad Networks"}
+                />
+              </div>
+
+              <div className="absolute top-[340px] right-[240px]">
+                <Bubble
+                  width={223}
+                  height={223}
+                  text={"Search Without Serfdom"}
+                />
+              </div>
+
+              <div className="absolute top-[450px] right-[0px]">
+                <Bubble width={175} height={175} text={"And more"} />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </section>
