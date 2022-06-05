ba7dd8f90d2091e1bc1f8a0d4694ab423d69dc9e
somali0128 <66934242+somali0128@users.noreply.github.com>
2022-06-05 16:03:35 -0300

    Analytic/ga (#20)
    * add gatag
    
    * fix lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,35 @@
 import Layout from "@/components/layout";
 import type { AppProps } from "next/app";
-
+import Script from "next/script";
 import "styles/globals.css";
 import "styles/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-GRG9CP511Z"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+   
+     gtag('config', 'G-GRG9CP511Z');
+    `}
+      </Script>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymus"
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
