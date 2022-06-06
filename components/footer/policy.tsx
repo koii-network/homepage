5548ddd534632366e369e551f4306c192bb848a8
Wojciech Jelen <w.jelen20@gmail.com>
2022-06-07 00:22:26 +0200

    fix

--- components/footer/policy.tsx
+++ components/footer/policy.tsx
@@ -4,13 +4,6 @@ export const Policy = () => {
   return (
     <div className="mb-3 md:mb-0">
       <ul className="flex flex-wrap justify-center gap-2 md:gap-6">
-        <div className="w-[100%] md:w-max">
-          <FooterLink
-            title={"Contest Terms & Conditions"}
-            url={"https://koii.network/Contest_TC_-_Cheeky_Lil_Contest.pdf"}
-          />
-        </div>
-
         <FooterLink title={"Privacy Polic"} url={"/Privacy_Policy.pdf"} />
         <FooterLink title={"Terms of Service"} url={"/TOU_June_22_2021.pdf"} />
       </ul>
