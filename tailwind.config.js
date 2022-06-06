c648367280236d2da9aad152695fe7ac9679c451
Wojciech Jelen <w.jelen20@gmail.com>
2022-06-06 22:06:19 +0200

    footer mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     fontFamily: {
       sans: ["Sora", "sans-serif"],
     },
