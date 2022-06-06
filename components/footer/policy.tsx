c881a0168a39f108b82752e529c3589b354db44f
Wojciech Jelen <w.jelen20@gmail.com>
2022-06-06 22:06:29 +0200

    footer mobile

--- components/footer/policy.tsx
+++ components/footer/policy.tsx
@@ -2,12 +2,15 @@ import { FooterLink } from "./footer-link";
 
 export const Policy = () => {
   return (
-    <div>
-      <ul className="flex gap-6">
-        <FooterLink
-          title={"Contest Terms & Conditions"}
-          url={"https://koii.network/Contest_TC_-_Cheeky_Lil_Contest.pdf"}
-        />
+    <div className="mb-3 md:mb-0">
+      <ul className="flex flex-wrap justify-center gap-6">
+        <div className="w-[100%] md:w-max">
+          <FooterLink
+            title={"Contest Terms & Conditions"}
+            url={"https://koii.network/Contest_TC_-_Cheeky_Lil_Contest.pdf"}
+          />
+        </div>
+
         <FooterLink
           title={"Privacy Policy"}
           url={"https://koii.network/Privacy_Policy.pdf"}
