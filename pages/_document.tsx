d5f37115a22d944756eab7f386bcc871db9ffe59
somali0128 <ldydq970128@gmail.com>
2022-06-08 02:57:28 -0300

    Update card

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,14 +14,15 @@ class MyDocument extends Document {
           />
           <meta name="og:url" content="https://koii.network/" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            name="twitter:image"
-            content="https://www.koii.network/static/thumbnail.png"
-          />
+          <meta name="twitter:site" content="@KoiiNetwork"/>
           <meta name="twitter:title" content="Koii Network" />
           <meta
             name="twitter:description"
             content="Koii was founded on fairness, trust, and one immense objective: to give creators a space where they permanently own and earn the value they deserve from their content."
+          />
+          <meta
+            name="twitter:image"
+            content="https://www.koii.network/static/thumbnail.png"
           />
           <link rel="shortcut icon" href="/static/KoiiNetwork-logo_64.svg" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
