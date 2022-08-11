a9aaa6d40e24e730b113bd4134cc821b059aa8f5
somali0128 <ldydq970128@gmail.com>
2022-08-11 14:31:05 -0300

    desktop responsive

--- components/node/hero/index.tsx
+++ components/node/hero/index.tsx
@@ -10,14 +10,6 @@ export const Nodehero = () => {
       className={`heroNode h-[1230px] lg:h-[900px] pt-[4rem] lg:pt-[70px]`}
       id="myID"
     >
-      <div
-        className={`h-[1082px] lg:h-[525px] relative z-10`}
-        style={{
-          background: isBreakpoint
-            ? `url("/images/Node/hero_curve.svg") no-repeat bottom center / cover`
-            : `url("/images/Node/hero_curve.svg") no-repeat bottom center / cover`,
-        }}
-      >
         <div className="container ">
           <div className="flex flex-col pt-[8rem] lg:pt-[4rem] lg:pl-[5rem] ">
             <div className="Node-title text-[#ECFFFE] text-white font-semibold md:font-blod text-[30px] md:text-[44px] lg:text-[56px] w-[300px] lg:w-[587px]">
@@ -58,17 +50,10 @@ Get on the list now - it’s first come, first serve!
             </div>
           </div>
 
-          <div className="ease-in-out animate-[guy_15000ms_ease-in-out_infinite;] guy absolute z-20 w-[366.62px] h-[462.49px] lg:w-[466.62px] lg:h-[562.49px] right-[50%] translate-x-2/4 md:translate-x-[75%] lg:translate-x-[100%] 2xl:translate-x-[125%] bottom-[10rem] md:bottom-[12rem] lg:bottom-[-11rem]">
-            <Image src="/images/Node/hero_guy.svg" layout="fill" />
-          </div>
-          <div className="ease-in-out animate-[guitar_15000ms_ease-in-out_infinite;] guitar absolute z-20 w-[378.62px] h-[414.49px] lg:w-[531.62px] lg:h-[514.49px] right-[50%] translate-x-2/4 md:translate-x-[75%] lg:translate-x-[100%] bottom-[10rem] md:bottom-[10rem] lg:bottom-[-11rem]">
-            <Image src="/images/Node/hero_guitar.svg" layout="fill" />
-          </div>
-          <div className="ease-in-out animate-[paint_15000ms_ease-in-out_infinite;] paint absolute z-20 w-[462.62px] h-[532.49px] lg:w-[627.5px] lg:h-[632.89px] right-[50%] translate-x-[45%] md:translate-x-[80%] lg:translate-x-[95%] bottom-[7rem] md:bottom-[6rem] lg:bottom-[-11rem]">
-            <Image src="/images/Node/hero_paint.svg" layout="fill" />
+          <div className="absolute z-20 w-[869px] h-[721px] lg:right-0 lg:top-0">
+            <Image src="/images/node/hero.svg" layout="fill" />
           </div>
         </div>
-      </div>
     </div>
   );
 };
