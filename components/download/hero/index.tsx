9b52e24c341fa9d42129cd43f910ed1eca97b4f3
somali0128 <ldydq970128@gmail.com>
2022-08-12 14:38:54 -0300

    fix lint

--- components/download/hero/index.tsx
+++ components/download/hero/index.tsx
@@ -25,46 +25,58 @@ export const Downloadhero = () => {
           </div>
           <div className="flex flex-row lg:pr-[5rem] w-[100%] place-content-evenly">
             <div className="window text-center">
-            <Button
-              width="260px"
-              height="40px"
-              asLink
-              target="_blank"
-              href="https://share.hsforms.com/1kLtk8rfURZ-HY2xnKRTfCgc20dg"
-              className="px-6 py-3 w-[260px] bg-[#9BE7C4] text-koiiblue text-normal rounded-[32px] drop-shadow-lg mt-[3rem]"
-              label="Download for Windows"
-            />
-            <p className="text-[#ECFFFE] mt-[1rem] text-[12px]">For Windows11 or later.</p>
+              <Button
+                width="260px"
+                height="40px"
+                asLink
+                target="_blank"
+                href="https://share.hsforms.com/1kLtk8rfURZ-HY2xnKRTfCgc20dg"
+                className="px-6 py-3 w-[260px] bg-[#9BE7C4] text-koiiblue text-normal rounded-[32px] drop-shadow-lg mt-[3rem]"
+                label="Download for Windows"
+              />
+              <p className="text-[#ECFFFE] mt-[1rem] text-[12px]">
+                For Windows11 or later.
+              </p>
             </div>
             <div className="mac text-center">
-            <Button
-              width="260px"
-              height="40px"
-              asLink
-              target="_blank"
-              href="https://share.hsforms.com/1kLtk8rfURZ-HY2xnKRTfCgc20dg"
-              className="px-6 py-3 w-[260px] bg-[#9BE7C4] text-koiiblue text-normal rounded-[32px] drop-shadow-lg mt-[3rem]"
-              label="Download for Mac"
-            />
-            <p className="text-[#ECFFFE] mt-[1rem] text-[12px]">For Mac OS 11.5 or later.</p>
+              <Button
+                width="260px"
+                height="40px"
+                asLink
+                target="_blank"
+                href="https://share.hsforms.com/1kLtk8rfURZ-HY2xnKRTfCgc20dg"
+                className="px-6 py-3 w-[260px] bg-[#9BE7C4] text-koiiblue text-normal rounded-[32px] drop-shadow-lg mt-[3rem]"
+                label="Download for Mac"
+              />
+              <p className="text-[#ECFFFE] mt-[1rem] text-[12px]">
+                For Mac OS 11.5 or later.
+              </p>
             </div>
             <div className="linux text-center">
-            <Button
-              width="260px"
-              height="40px"
-              asLink
-              target="_blank"
-              href="https://share.hsforms.com/1kLtk8rfURZ-HY2xnKRTfCgc20dg"
-              className="px-6 py-3 w-[260px] bg-[#9BE7C4] text-koiiblue text-normal rounded-[32px] drop-shadow-lg mt-[3rem]"
-              label="Download for Linux"
-            />
-            <p className="text-[#ECFFFE] mt-[1rem] text-[12px]">For Linux ### or later.</p>
+              <Button
+                width="260px"
+                height="40px"
+                asLink
+                target="_blank"
+                href="https://share.hsforms.com/1kLtk8rfURZ-HY2xnKRTfCgc20dg"
+                className="px-6 py-3 w-[260px] bg-[#9BE7C4] text-koiiblue text-normal rounded-[32px] drop-shadow-lg mt-[3rem]"
+                label="Download for Linux"
+              />
+              <p className="text-[#ECFFFE] mt-[1rem] text-[12px]">
+                For Linux ### or later.
+              </p>
             </div>
           </div>
           <div className="download-subtitle text-[#ECFFFE] text-white text-[18px] md:text-[20px] lg:text-[20px] md:w-[400px] lg:w-[600px] w-[280px] pt-[4rem] leading-[32px]">
             <p>Having trouble?</p>
-            <p>Check out the 
-                <Link href="/node"><a><span className="underline"> support page</span></a></Link></p>
+            <p>
+              Check out the
+              <Link href="/node">
+                <a>
+                  <span className="underline"> support page</span>
+                </a>
+              </Link>
+            </p>
           </div>
         </div>
         {isBreakpoint ? (
