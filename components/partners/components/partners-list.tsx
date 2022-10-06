0b90d1087d22d1ad4d3a7d3dc682aefc9eacbb92
somali0128 <ldydq970128@gmail.com>
2022-10-06 17:45:40 -0300

    fix responsive

--- components/partners/components/partners-list.tsx
+++ components/partners/components/partners-list.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-restricted-imports
 import { useMediaQuery } from "@/components/hooks";
+import { partnersLinks } from "@/config/partners-links";
 import { partnersFirstRow } from "@/config/partners-links";
 import { partnersSecondtRow } from "@/config/partners-links";
 
@@ -12,9 +13,9 @@ export const PartnersList = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center">
+      {isLgBreakpoint ? (
         <div className="flex z-40 flex-wrap justify-center w-[90%] md:w-[480px] lg:w-[670px] md:gap-2 lg:gap-4 lg:gap-4 xl:gap-x-10 mb-[10px] md:mb-[20px]  lg:mb-[30px]">
-          {partnersFirstRow.map(({ name, iconSrc, url }) => (
+          {partnersLinks.map(({ name, iconSrc, url }) => (
             <Iconlink
               url={url}
               iconSource={iconSrc}
@@ -24,18 +25,32 @@ export const PartnersList = () => {
             />
           ))}
         </div>
-        <div className="flex z-40 flex-wrap justify-center w-[90%] md:w-[640px] lg:w-[770px] md:gap-2 lg:gap-4 lg:gap-4 xl:gap-x-4 mb-[30px] md:mb-[30px]  lg:mb-[68px]">
-          {partnersSecondtRow.map(({ name, iconSrc, url }) => (
-            <Iconlink
-              url={url}
-              iconSource={iconSrc}
-              key={name}
-              width={iconSize}
-              height={iconSize}
-            />
-          ))}
+      ) : (
+        <div className="flex flex-col items-center">
+          <div className="flex z-40 flex-wrap justify-center w-[90%] md:w-[480px] lg:w-[670px] md:gap-2 lg:gap-4 lg:gap-4 xl:gap-x-10 mb-[10px] md:mb-[20px]  lg:mb-[30px]">
+            {partnersFirstRow.map(({ name, iconSrc, url }) => (
+              <Iconlink
+                url={url}
+                iconSource={iconSrc}
+                key={name}
+                width={iconSize}
+                height={iconSize}
+              />
+            ))}
+          </div>
+          <div className="flex z-40 flex-wrap justify-center w-[90%] md:w-[640px] lg:w-[770px] md:gap-2 lg:gap-4 lg:gap-4 xl:gap-x-4 mb-[30px] md:mb-[30px]  lg:mb-[68px]">
+            {partnersSecondtRow.map(({ name, iconSrc, url }) => (
+              <Iconlink
+                url={url}
+                iconSource={iconSrc}
+                key={name}
+                width={iconSize}
+                height={iconSize}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
