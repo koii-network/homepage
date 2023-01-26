24764cc80dec5697cc41c8d941bf3a4182fb9dd9
Fafemi Adeola <adeola5678@gmail.com>
2023-01-26 04:32:54 +0100

    fix: comments

--- components/founders/hero/index.tsx
+++ components/founders/hero/index.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/Button";
-import ReactPlayer from "react-player";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 export const Hero = () => {
   return (
-    <div className="bg-mint pt-40 relative flex justify-center">
+    <div className="bg-lightmint pt-40 relative flex justify-center">
       <div className="relative px-8 w-full flex flex-col justify-center items-center">
         <div className="relative pb-80 flex z-30 flex-col justify-center items-center">
           <div className="relative w-[238px] h-[269px] mt-[-22px]">
@@ -13,15 +14,15 @@ export const Hero = () => {
               alt="desktop-node"
               layout="fill"
               objectFit="contain"
-            ></Image>
+            />
           </div>
-          <p className="pt-[30px] text-[32px] leading-10 text-white font-sans font-semibold">
+          <span className="pt-[30px] text-[32px] leading-10 text-white font-sans font-semibold">
             Reimagine the web: Bring your passion project to life.
-          </p>
-          <p className="pt-7 text-base leading-[22px] w-full text-white font-sans">
+          </span>
+          <span className="pt-7 text-base leading-[22px] w-full text-white font-sans">
             Calling all founders with a revolutionary idea. <br />
             Koii’s tools and support program empower you to make it happen fast.
-          </p>
+          </span>
           <Button
             label=" Join the Next Cohort"
             className="text-koiipurple text-base tracking-wider px-6 py-3 bg-[#F5F5F5] mt-10 rounded-md font-sans"
@@ -41,7 +42,7 @@ export const Hero = () => {
               src={`/images/founders/hero-background.png`}
               alt="desktop-node"
               layout="fill"
-            ></Image>
+            />
           </div>
         </div>
       </div>
