5d4a1bc75408c131644b2ef7669b3dfda75ad627
Fafemi Adeola <adeola5678@gmail.com>
2023-01-30 18:53:08 +0100

    fix: desktop screen

--- components/founders/program-benefits/benefit.tsx
+++ components/founders/program-benefits/benefit.tsx
@@ -1,5 +1,6 @@
 import Image, { StaticImageData } from "next/image";
 import { useInView } from "react-intersection-observer";
+import { useMediaQuery } from "@/components/hooks";
 import styles from "/styles/benefit.module.scss";
 
 interface BenefitProps {
@@ -16,19 +17,22 @@ export const Benefit = ({ image, text, side, lastIndex }: BenefitProps) => {
     rootMargin: "-30% 0% -45% 0%",
   });
 
-  const benefitClasses = ` flex gap-5 mb-4 items-center flex-shrink-0 ${
-    side === "left" ? "flex-row text-right" : "flex-row-reverse"
+  const isBreakpoint = useMediaQuery(1024);
+  const benefitClasses = `flex gap-5 mb-4 items-center flex-shrink-0 ${
+    side === "right" && isBreakpoint
+      ? "flex-row-reverse"
+      : "flex-row text-right"
   }
   ${lastIndex ? (side === "left" ? "-ml-10 sm:m-0" : "-mr-24 sm:m-0") : ""}`;
 
   const imageContainer = `${styles.container} flex-shrink-0`;
 
   const imageClasses = `transition-all ease-in-out duration-300 ${
-    inView ? "drop-shadow-3xl" : ""
+    inView && isBreakpoint ? "drop-shadow-3xl" : ""
   }`;
 
   const textClasses = `font-sans transition-all text-lg leading-6 tracking-tighter ease-in-out duration-200 ${
-    inView ? "text-mint font-bold" : "text-white"
+    inView && isBreakpoint ? "text-mint font-bold" : "text-white"
   } ${side === "left" ? "text-left" : "text-right"}`;
 
   return (
