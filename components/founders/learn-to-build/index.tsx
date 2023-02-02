4023a94dd9933593e2ebd6b4216d7ecd7883cd00
Pablo Poggio <pablo@koii.network>
2023-02-02 14:03:35 -0300

    Minor tweak to Learn to Build's title

--- components/founders/learn-to-build/index.tsx
+++ components/founders/learn-to-build/index.tsx
@@ -2,14 +2,13 @@ import { Product } from "./product";
 import { products } from "@/config/products-to-build-on-koii";
 
 export const LearnToBuild = () => (
-  <div className="relative flex flex-col items-center gap-10 -mt-48 bg-lightmint pt-32 pb-96 rounded-t-full text-purple text-center font-medium">
-    <h3 className="text-2xl">
+  <div className="relative flex flex-col items-center bg-white pb-96 text-center font-medium text-purple">
+    <h3 className="section-bottom-transition md:section-bottom-transition-desktop mb-12 h-64 w-full bg-[#1e1d5a] text-2xl text-lightmint lg:bg-[#191854] lg:text-[40px] lg:text-white">
       Community Ownership & Governance:
       <br />
       The future is in your hands.
     </h3>
 
-    <p className="text-">LEARN TO BUILD:</p>
     {products.map(({ label, image, text }, index) => (
       <Product
         key={label}
