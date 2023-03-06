a866dffa5546f36cf439ffe1791c1802986a20da
Soma Li <soma@koii.network>
2023-03-06 14:12:59 -0400

    fix lint

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -8,7 +8,8 @@ import { NewsletterButton } from "./newsletter-button";
 export const Navbar = () => {
   const [openMobileMenu, setOpenMobileMenu] = useState(false);
   const toggleOpenMenu = () => {
-    setOpenMobileMenu(!openMobileMenu);  };
+    setOpenMobileMenu(!openMobileMenu);
+  };
 
   const wrapperRef = useRef(null);
   useOutsideAlerter(wrapperRef);
