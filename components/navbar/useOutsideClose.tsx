208eb334e2ee3c7ec6ea4b5c3b4dc0e104e3f9e3
Soma Li <soma@koii.network>
2023-03-06 23:47:30 -0400

    fix lint

--- components/navbar/useOutsideClose.tsx
+++ components/navbar/useOutsideClose.tsx
@@ -1,17 +1,16 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useEffect } from "react";
 
 export default function useOutsideClose(ref: any) {
-    useEffect(() => {
-        function handleClickOutside(event: any) {
-          if (ref.current && !ref.current.contains(event.target)) {
-            
-          }
-        }
-        // Bind the event listener
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-          // Unbind the event listener on clean up
-          document.removeEventListener("mousedown", handleClickOutside);
-        };
-      }, [ref]);
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
 }
